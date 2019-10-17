5bcf135860919178850ea7bfa2c515bc0e97677a
Oliver Huang <56331408+Oliverh1982@users.noreply.github.com>
2019-10-17 15:40:55 +0800

    getting data from rossmax sa310
    getting data from rossmax sa310; shuld write  01 to handle 0x0046, and handle 0x0046 is a notification which will send data automatically.

--- rossmax_sa310.py
+++ rossmax_sa310.py
@@ -8,5 +8,29 @@ child.sendline("connect")
 child.expect("Connection successful", timeout=10)
 print(" Connected!")
 
+time.sleep(1)
+child.sendline("char-write-req 0x0047 0100")
+print(" Write cmd!")
 
+while True:
+     try:
+      child.expect("Notification handle = 0x0046 value: ", timeout=2)
+      child.expect("\r\n", timeout=2)
+
+      print(child.before)
+      #value=bytes.decode(child.before)
+      #print('str value=',value)
+      
+      spo2=child.before[21:23]
+      pulse=child.before[24:26]
+     
+      spo2_value=int(spo2,16)
+      pulse_value=int(pulse,16) 
+      print()
+      print('Spo2 is %d'%spo2_value,'%')
+      print('Pulse is %d /min'%pulse_value)
+
+     except pexpect.TIMEOUT:
+       print('waiting.....')
+       pass
 
